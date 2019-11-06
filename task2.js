8414cf007885be649a2efeda498e2b2a743bd074
Baytik Tumanbaev <jumka.wows@gmail.com>
2019-11-06 16:22:18 +0600

    first commit

---
+++ task2.js
@@ -0,0 +1,14 @@
+var figlet = require("figlet");
+var arg = process.argv[2];
+
+figlet.text(arg, function(error, data) {
+
+    if (error)
+
+        console.log(error);
+
+    else
+
+        console.log(data);
+
+});
